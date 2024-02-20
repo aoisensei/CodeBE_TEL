6e8dcec5d98be2511baf66f7fe309f6766497907
ThanhNLT2 <thanhnlt2@fpt.com>
2024-02-20 23:26:56 +0700

    add DTO class

--- CodeBE_TEL/Controllers/ClassroomController/Classroom_ClassroomDTO.cs
+++ CodeBE_TEL/Controllers/ClassroomController/Classroom_ClassroomDTO.cs
@@ -1,6 +1,39 @@
-﻿namespace CodeBE_TEL.Controllers.ClassroomController
+﻿using CodeBE_TEL.Entities;
+
+namespace CodeBE_TEL.Controllers.ClassroomController
 {
     public class Classroom_ClassroomDTO
     {
+        public long Id { get; set; }
+
+        public long AppUserId { get; set; }
+
+        public string Code { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public List<Classroom_ClassEventDTO>? ClassEvents { get; set; }
+
+        public Classroom_ClassroomDTO() { }
+        public Classroom_ClassroomDTO(Classroom Classroom)
+        {
+            Id = Classroom.Id;
+            AppUserId = Classroom.AppUserId;
+            Code = Classroom.Code;
+            Name = Classroom.Name;
+            Description = Classroom.Description;
+            CreatedAt = Classroom.CreatedAt;
+            UpdatedAt = Classroom.UpdatedAt;
+            DeletedAt = Classroom.DeletedAt;
+            ClassEvents = Classroom.ClassEvents.Select(x => new Classroom_ClassEventDTO(x)).ToList();
+        }
     }
 }
---
+++ CodeBE_TEL/Controllers/ClassroomController/Classroom_CommentDTO.cs
@@ -0,0 +1,22 @@
+﻿using CodeBE_TEL.Entities;
+
+namespace CodeBE_TEL.Controllers.ClassroomController
+{
+    public class Classroom_CommentDTO
+    {
+        public long Id { get; set; }
+
+        public long ClassEventId { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public Classroom_CommentDTO() { }
+        public Classroom_CommentDTO(Comment Comment)
+        {
+            Id = Comment.Id;
+            ClassEventId = Comment.ClassEventId;
+            Description = Comment.Description;
+        }
+
+    }
+}
